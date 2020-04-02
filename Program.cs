196300884d9f78c231d2a546caad419c6eccbaa0
Noelle Caldwell <noecald@microsoft.com>
2020-04-02 14:07:36 -0700

    added new line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SampleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New line");
         }
     }
 }
