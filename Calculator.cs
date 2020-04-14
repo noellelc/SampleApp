095d80ff0d2c7d1f1f1717345998faa8f09aa820
Noelle Caldwell <noelle.caldwell@microsoft.com>
2020-04-14 09:46:32 -0700

    Test branch (#5)
    * added nuget.config
    
    * squashing them
    
    * add multiply
    
    * add divide
    
    * add calls

--- Calculator.cs
+++ Calculator.cs
@@ -11,5 +11,15 @@ namespace SampleApp
         {
             return a - b;
         }
+
+        public int Multiply(int a, int b) 
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            return a/b;
+        }
     }
 }
