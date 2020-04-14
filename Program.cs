fc8466c644d9178c43697140c9da6efd2c5e2958
noecald <noelle.caldwell@microsoft.com>
2020-04-14 09:44:56 -0700

    add calls

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace SampleApp
 
             Console.WriteLine($"2 + 3 = {calc.Add(2,3)}");
             Console.WriteLine($"12 - 3 = {calc.Subtract(12,3)}");
+            Console.WriteLine($"12 * 3 = {calc.Multiply(12,3)}");
+            Console.WriteLine($"12 / 3 = {calc.Divide(12,3)}");
         }
     }
 }
